a93bdb7905250e479755a90e27055498c41b08ab
Geraldo Felipe <lordfelipe@gmail.com>
2015-07-14 17:11:46 -0300

    update and minify controllers

--- public/javascripts/app/controllers/IndexController.js
+++ public/javascripts/app/controllers/IndexController.js
@@ -4,27 +4,6 @@ vulpe.ng.app.controller('IndexController', ['$rootScope', '$scope', 'VulpeJS', f
 
   vulpejs.timeout.cancel();
 
-  var re_weburl = new RegExp(
-    "^" +
-    "(?:(?:https?|ftp)://)" +
-    "(?:\\S+(?::\\S*)?@)?" +
-    "(?:" +
-    "(?!(?:10|127)(?:\\.\\d{1,3}){3})" +
-    "(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})" +
-    "(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})" +
-    "(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])" +
-    "(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}" +
-    "(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))" +
-    "|" +
-    "(?:(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)" +
-    "(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*" +
-    "(?:\\.(?:[a-z\\u00a1-\\uffff]{2,}))" +
-    ")" +
-    "(?::\\d{2,5})?" +
-    "(?:/\\S*)?" +
-    "$", "i"
-  );
-
   vulpejs.item = {
     url: ''
   };
@@ -93,7 +72,7 @@ vulpe.ng.app.controller('IndexController', ['$rootScope', '$scope', 'VulpeJS', f
   vulpejs.send = function() {
     vulpejs.message.clean();
     if (vulpejs.item.url && vulpejs.item.url.length > 0) {
-      if (!re_weburl.test(vulpejs.item.url)) {
+      if (!vulpe.utils.regex.weburl.test(vulpejs.item.url)) {
         vulpejs.message.error('Please, enter a valid URL.');
         focus();
         return;
